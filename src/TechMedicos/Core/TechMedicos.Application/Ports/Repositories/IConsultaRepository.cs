3c81e37e58b15bd1f3199c461a1afd9cb73a9143
vitor <vi.lupinetti@hotmail.com.br>
2024-07-21 20:57:15 -0300

    criação interfaces repositories

---
+++ src/TechMedicos/Core/TechMedicos.Application/Ports/Repositories/IConsultaRepository.cs
@@ -0,0 +1,12 @@
+﻿using TechMedicos.Core;
+using TechMedicos.Domain.Aggregates;
+
+namespace TechMedicos.Application.Ports.Repositories
+{
+    public interface IConsultaRepository:IRepository<Consulta>
+    {
+        Task<Consulta> Cadastrar(Consulta consulta);
+        Task<Consulta> ObterPorId(string consultaId);
+        Task<Consulta> Atualizar(Consulta consulta);
+    }
+}
---
+++ src/TechMedicos/Core/TechMedicos.Application/Ports/Repositories/IMedicoRepository.cs
@@ -0,0 +1,14 @@
+﻿using TechMedicos.Core;
+using TechMedicos.Domain.Aggregates;
+
+namespace TechMedicos.Application.Ports.Repositories
+{
+    public interface IMedicoRepository:IRepository<Medico>
+    {
+        Task<List<Medico>> ObterTodos();
+        Task<Medico> ObterPorId(string medicoId);
+        Task<Medico> Cadastrar(Medico medico);
+        Task<Medico> Atualizar(Medico medico);
+        Task Deletar(string medicoId);
+    }
+}
