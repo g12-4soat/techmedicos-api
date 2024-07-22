ca3d312c17d185c57ec601d6476aad9c26e5ce6b
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2024-07-21 23:04:56 -0300

    Movendo tipo de horario disponivel

--- src/TechMedicos/Core/TechMedicos.Domain/ValueObjects/AgendamentoMedico.cs
+++ src/TechMedicos/Core/TechMedicos.Domain/ValueObjects/AgendamentoMedico.cs
@@ -48,22 +48,4 @@ namespace TechMedicos.Domain.ValueObjects
             yield return Horarios;
         }
     }
-
-    public class HorarioDisponivel : ValueObject
-    {
-        public HorarioDisponivel(TimeOnly horaInicio)
-        {
-            HoraInicio = horaInicio;
-            HoraFim = horaInicio.AddMinutes(50);
-        }
-
-        public TimeOnly HoraInicio { get; private set; }
-        public TimeOnly HoraFim { get; private set; }
-
-        protected override IEnumerable<object> RetornarPropriedadesDeEquidade()
-        {
-            yield return HoraInicio;
-            yield return HoraFim;
-        }
-    }
 }
