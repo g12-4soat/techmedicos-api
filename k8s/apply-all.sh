46041c21d9deace89eac627cbbc544bb15793c36
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2024-07-23 13:23:28 -0300

    Movendo manifestos k8s

--- k8s/apply-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# For Unix-like operating systems (Linux Distros, Mac OS ...)
-# /> chmod +x apply-all.sh
-# Apply all files (including subdirectories)
-kubectl apply -f ./k8s/metrics/metrics.yaml
-kubectl apply -f ./k8s/techmedicos-namespace.yaml
-kubectl apply -f ./k8s/techmedicos-configmap.yaml
-kubectl apply -f ./k8s/deployments/techmedicos-api-deployment.yaml
